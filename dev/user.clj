9f25b822411ffdb066e9bfa58ba94b25c1064590
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2024-10-30 13:16:51 +0100

    Comment out the sleep call

--- dev/user.clj
+++ dev/user.clj
@@ -7,5 +7,9 @@
   ;; NOTE: if we don't delay this, the message about starting the
   ;; `sci.nrepl` server will be printed first and thus cider will
   ;; connect to that port instead of the clojure one
-  (Thread/sleep 1000)
+
+  ;; (Thread/sleep 1000)
+  ;; this is not needed when using
+  ;; clerk-render-repl-cider-jack-in-clj&cljs (v1)
+
   (clerk/serve! {:browse true}))
